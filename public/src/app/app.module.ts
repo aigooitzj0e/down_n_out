420727c61d3640fe6aa404545bbedc0002270d3d
Joe Yu <joeyu@Joes-MacBook-Pro.local>
2017-09-26 16:22:54 -0700

    add blah blah blah to google-maps.component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAxAh9XAJJr_qDteIBpJt50ODd1Ojw0qIo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
